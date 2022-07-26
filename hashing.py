74072fc8b74bcf698f8aff13fbe77358fe9534a5
aditya narayanan <106238511+narayanan2000@users.noreply.github.com>
2022-07-26 11:42:08 +0530

    first commit

---
+++ hashing.py
@@ -0,0 +1,15 @@
+arr = [1, 2, 3, 4, 5, 6, 8, 5, 3, 67, 3, 674, 87]
+dictionary = {}
+max_val = 0
+max_num = ""
+for x in arr:
+    if str(x) not in dictionary:
+        dictionary[str(x)] = 1
+    else:
+        dictionary[str(x)] += 1
+    if dictionary[str(x)] > max_val:
+        max_val = dictionary[str(x)]
+        max_num = str(x)
+
+print(max_num)
+print(dictionary)
